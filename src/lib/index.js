7365375213d7a5d29f27f7711b20410f8e469629
Adrià <adriablancafort@gmail.com>
2025-05-17 18:59:24 +0200

    add async demo

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,1 +1,5 @@
-// place files you want to import through the `$lib` alias in this folder.
+export function delayed(value, ms = 500) {
+	return new Promise((f) => {
+		setTimeout(() => f(value), ms);
+	});
+}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,14 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+	import { delayed } from '$lib/index.js';
+	
+	let count = $state(0);
+</script>
+
+<svelte:boundary>
+	<button onclick={() => count += 1}>increment</button>
+	<p>{count} * 2 = {await delayed(count * 2)}</p>
+	
+	{#snippet pending()}
+		<p>loading...</p>
+	{/snippet}
+</svelte:boundary>

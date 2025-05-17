aed8bd9f9241aaecf69179a3ee4b5000b65d29b8
Adrià <adriablancafort@gmail.com>
2025-05-17 19:27:48 +0200

    remove svelte:boundary

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,25 +8,19 @@
 
 <h1>Product List</h1>
 
-<svelte:boundary>
-    {#await fetchProducts()}
-        <p>Loading products...</p>
-    {:then data}
-        <p>Showing {data.products.length} of {data.total} products.</p>
-        <ul class="list-none p-0">
-            {#each data.products as product (product.id)}
-                <li class="border border-gray-200 p-4 mb-4">
-                    <h2>{product.title}</h2>
-                    <p>{product.description}</p>
-                    <p>Price: ${product.price}</p>
-                </li>
-            {/each}
-        </ul>
-    {:catch error}
-        <p style="color: red;">Error loading products: {error.message}</p>
-    {/await}
-
-    {#snippet pending()}
-        <p>Boundary is working...</p>
-    {/snippet}
-</svelte:boundary>
+{#await fetchProducts()}
+    <p>Loading products...</p>
+{:then data}
+    <p>Showing {data.products.length} of {data.total} products.</p>
+    <ul class="list-none p-0">
+        {#each data.products as product (product.id)}
+            <li class="border border-gray-200 p-4 mb-4">
+                <h2>{product.title}</h2>
+                <p>{product.description}</p>
+                <p>Price: ${product.price}</p>
+            </li>
+        {/each}
+    </ul>
+{:catch error}
+    <p>Error loading products: {error.message}</p>
+{/await}
